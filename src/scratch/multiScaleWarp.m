0a5c52d9fd60a291192c6e6f255306657c58381b
Andreea Bobu <abobu@cassia.csail.mit.edu>
2015-05-31 15:16:28 -0400

    pushed multiscale warp

--- src/scratch/multiScaleWarp.m
+++ src/scratch/multiScaleWarp.m
@@ -3,8 +3,8 @@ function multiScaleWarp(exid)
     %Then call patchmrf and use patchlib.correspdst for the pair potentials. 
     %Then repeat
     
-    W = 256;
-    H = 256;
+    W = 60;
+    H = 60;
     source = zeros(W, H);
     if exid == 1
         source(30, 30) = 1; % get an image with a bump
@@ -12,33 +12,33 @@ function multiScaleWarp(exid)
         target = volwarp(source, ID); % move the bump by DX, DY
     elseif exid == 2
         source = rand(W, H);
-        [target, ~] = sim.ovoidShift(source, 50, false);
+        [target, ID] = sim.ovoidShift(source, 50, false);
     elseif exid == 3
         %cat.jpg example
-        imd = im2double(rgb2gray(imread('C:\Users\Andreea\Dropbox (MIT)\MIT\Sophomore\Spring\UROP\cat.jpg')));
+        imd = im2double(rgb2gray(imread('/afs/csail.mit.edu/u/a/abobu/toolbox/cat.jpg')));
         source = volresize(imd, [W, H]);
-        [target, ~] = sim.randShift(source, 1, 4, 4, false);
+        [target, ID] = sim.randShift(source, 3, 20, 20, false);
     elseif exid == 4
         %Real example
-        nii = loadNii('C:\Users\Andreea\Dropbox (MIT)\MIT\Sophomore\Spring\UROP\robert\0002_orig.nii');
+        nii = loadNii('/afs/csail.mit.edu/u/a/abobu/toolbox/robert/0002_orig.nii');
         source = volresize(double(nii.img(:, :, 100)), [81, 81]);
-        [target, ~] = sim.randShift(source, 3, 4, 4, false);
+        [target, ID] = sim.randShift(source, 3, 4, 4, false);
     elseif exid == 5
         % Checkboard example
-        source = checkerboard(10, 6, 6);
-        [target, ID] = sim.randShift(source, 3, 6, 6, false);
+        source = checkerboard(10, 3, 3);
+        [target, ID] = sim.randShift(source, 3, 20, 20, false);
     else
         %manual image quadrants
         [xx, yy] = ndgrid(1:W, 1:H);
         source = 1*(xx >= W/2 & yy >= H/2) + 0.33*(xx < W/2 & yy >= H/2) + 0.66*(xx >= W/2 & yy < H/2);
-        [target, ~] = sim.ovoidShift(source, 6, false);
+        [target, ID] = sim.ovoidShift(source, 6, false);
     end   
     
     patchSize = [3, 3];
     patchOverlap = 'sliding';
     disp = {zeros(size(source)), zeros(size(source))};
     
-    for s = 30:size(source, 1)
+    for s = 10:2:size(source, 1)
         s
         % resizing the original source and target images to s
         targetS = volresize(target, [s, s]);
