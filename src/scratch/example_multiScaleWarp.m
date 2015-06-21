34d9bfd06570674704d520844033028fa2640e7e
Andreea Bobu <abobu@cassia.csail.mit.edu>
2015-06-21 19:53:29 -0400

    pushed all changes

--- src/scratch/example_multiScaleWarp.m
+++ src/scratch/example_multiScaleWarp.m
@@ -3,8 +3,8 @@ function [sourceWarped, displ] = example_multiScaleWarp(exid)
     % parameters
     patchSize = [3, 3];
     patchOverlap = 'sliding';
-    nScales = 5;
-    nInnerReps = 2;
+    nScales = 10;
+    nInnerReps = 1;
     warning off backtrace; % turn off backtrace for warnings.
     
     W = 128;
