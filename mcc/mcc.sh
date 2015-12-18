73e14f3f93b882a3c207900092a353a2d52a5845
adalca <adalca@mit.edu>
2015-12-18 13:20:26 -0500

    adding mcc

---
+++ mcc/mcc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# mcc files relevant topatch registration project.
+
+# prepare project and toolbox paths
+# if want to make current: "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MAIN_PATH="/data/vision/polina/users/adalca/patchRegistration"
+PROJECT_PATH="${MAIN_PATH}/git/"
+TOOLBOX_PATH="/data/vision/polina/users/adalca/MATLAB/toolboxes"
+
+# MCC-related paths
+MCCBUILD_PATH="${TOOLBOX_PATH}/mcc/" # mccBuild script path
+MCC_RUN_DIR="/afs/csail.mit.edu/system/common/matlab/2013b/bin/mcc"
+
+# need to add main path to system path (?).
+export PATH="${MAIN_PATH}:$PATH"
+
+## run mcc on desired (*.m) files.
+for filename in registerBuckner
+do
+  # run via mccBuild.sh
+  ${MCCBUILD_PATH}/mccBuild.sh \
+    ${MCC_RUN_DIR} \
+    ${PROJECT_PATH}/clust/${filename}.m \
+    ${MAIN_PATH}/MCC/MCC_${filename} \
+    ${PROJECT_PATH}/src/ \
+    ${TOOLBOX_PATH}
+done
