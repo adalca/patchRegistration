e53b261fd35a875bce3e6e684fd9376340cfaf48
Adrian Dalca <adalca@hops.csail.mit.edu>
2016-01-06 15:37:15 -0500

    add stats sge

--- sge/sgeStatsBuckner.sh
+++ sge/sgeStatsBuckner.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # run statistics on buckner registration
 
-# prepare SGE variables
-export SGE_LOG_PATH=/data/vision/polina/scratch/adalca/patchSynthesis/sge/
-export SGE_O_PATH=${SGE_LOG_PATH}
-export SGE_O_HOME=${SGE_LOG_PATH}
-mkdir -p $SGE_LOG_PATH
-echo $SGE_O_HOME
+###############################################################################
+# Settings
+###############################################################################
 
 # MCR file. This has to match the MCC version used in mcc.sh
 mcr=/data/vision/polina/shared_software/MCR/v82/
@@ -18,11 +15,15 @@ OUTPUT_PATH="/data/vision/polina/scratch/patchRegistration/output/";
 PROJECT_PATH="/data/vision/polina/users/adalca/patchRegistration/git/"
 CLUST_PATH="/data/vision/polina/users/adalca/patchRegistration/MCC/";
 
-# this version's running path
-runver="span_le_ds";
-
 # command shell file
-mccSh="${CLUST_PATH}MCC_registerBuckner/run_reg2stats.sh"
+mccSh="${CLUST_PATH}MCC_registerBuckner/run_registerBuckner.sh"
+
+# this version's running path
+runver="span_at4Scales_lambdaedge_gridspacing_innerreps";
+
+###############################################################################
+# Running Code
+###############################################################################
 
 # execute
 veroutpath="${OUTPUT_PATH}/runs_${runver}/"
@@ -30,13 +31,20 @@ for subjfolder in "${veroutpath}/*"
 do
   infolder = "${veroutpath}/${subjfolder}/"
   outfile = "${infolder}/stats.mat"
+  lcmd="${mccSh} $mcr $infolder $outfile"
 
-  sgeopath = "${infolder}/sge/"
-  mkdir -p $sgeopath
-
-  cmd="${PROJECT_PATH}sge/qsub-run ${mccSh} $mcr $infolder $outfile -o $sgeopath -e $sgeopath"
+  # create sge file
+  sgerunfile = "${infolder}/sge/reg2stats.sh"
+  sge_par_o="--sge \"-o ${sgeopath}\""
+  sge_par_e="--sge \"-e ${sgeopath}\""
+  cmd="${PROJECT_PATH}sge/qsub-run -c $sge_par_o $sge_par_e ${lcmd} > ${sgerunfile}"
   echo $cmd
-  $cmd
+  eval $cmd
+
+  # run sge
+  sgecmd="qsub ${sgerunfile}"
+  echo $sgecmd
+  $sgecmd
 
   sleep 1
 done
