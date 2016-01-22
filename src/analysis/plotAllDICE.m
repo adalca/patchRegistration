c20ace98c80188220672740d860247e871fc323a
Andreea Bobu <abobu@hops.csail.mit.edu>
2016-01-22 00:04:48 -0500

    plot code

--- src/analysis/plotAllDICE.m
+++ src/analysis/plotAllDICE.m
@@ -1,9 +1,10 @@
 fpath = ['/data/vision/polina/scratch/patchRegistration/output/', ...
-'runs_sparse_span_at4Scales_lambdaedge_gridspacing_innerreps/buckner*'];
+'runs_sparse_v2_span_at4Scales_lambdaedge_gridspacing_innerreps/buckner*'];
 d = sys.fulldir(fpath);
 
 paramIdx = [17, 2, 3, 4, 53, 41, 42, 43];
 diceScores = nan(8, numel(d));
+times = nan(1, numel(d));
 jaccardScores = nan(8, numel(d));
 idx = nan(numel(d), 4);
 
@@ -15,7 +16,7 @@ for i = 1:numel(d)
     znr = str2num(z{1}(end-1:end));
 
     try
-        n00 = load(fullfile(d(i).name, 'out', '0_0.mat'), 'params');
+        n00 = load(fullfile(d(i).name, 'out', '0_0.mat'), 'params', 'mastertoc');
         sta = load(fullfile(d(i).name, 'out', 'stats.mat'), 'dices', 'dicelabels', 'jaccards', 'jaccardlabels');
     catch
         fprintf('skipping %s\n', fname);
@@ -27,6 +28,7 @@ for i = 1:numel(d)
     idx(i, 2) = n00.params.mrf.lambda_edge;
     idx(i, 3) = n00.params.gridSpacing(end);
     idx(i, 4) = n00.params.nInnerReps;
+    times(i) = n00.mastertoc;
     for p = 1:numel(paramIdx)
         diceScores(p, i) = sta.dices{n00.params.nScales, end}(sta.dicelabels{n00.params.nScales, end}(:) == paramIdx(p));
         jaccardScores(p, i) = 1 - sta.jaccards{n00.params.nScales, end}(sta.jaccardlabels{n00.params.nScales, end}(:) == paramIdx(p));
@@ -38,12 +40,17 @@ end
 dicePlotLabels = {'dice LeftHippocampus', 'dice LeftCerebralWhiteMatter', 'dice LeftCerebralCortex', 'dice LeftLateralVentricle', 'dice RightHippocampus', 'dice RightCerebralWhiteMatter', 'dice RightCerebralCortex', 'dice RightLateralVentricle'};
 jaccardPlotLabels = {'jaccard LeftHippocampus', 'jaccard LeftCerebralWhiteMatter', 'jaccard LeftCerebralCortex', 'jaccard LeftLateralVentricle', 'jaccard RightHippocampus', 'jaccard RightCerebralWhiteMatter', 'jaccard RightCerebralCortex', 'jaccard RightLateralVentricle'};
 
+figure();
+boxplot(times, idx(:,2)); hold on; grid on;
+figure();
+boxplot(times, idx(:,4)); hold on; grid on;
+
 for param = 1:4
     f = figure();
     for labelIdx = 1:numel(paramIdx)
         subplot(2, 4, labelIdx);
-        boxplot(diceScores(labelIdx, :), idx(:,param)); hold on;
-        if param==2
+        boxplot(diceScores(labelIdx, :), idx(:,param)); hold on; grid on;
+        if param==2 || param==4
             ylim([0.3, 1]);
         end
     end
@@ -58,8 +65,8 @@ for param = 1:4
     f = figure();
     for labelIdx = 1:numel(paramIdx)
         subplot(2, 4, labelIdx);
-        boxplot(jaccardScores(labelIdx, :), idx(:,param)); hold on;
-        if param==2
+        boxplot(jaccardScores(labelIdx, :), idx(:,param)); hold on; grid on;
+        if param==2 || param==4
             ylim([0.3, 1]);
         end
     end
@@ -67,8 +74,5 @@ for param = 1:4
     ax = findobj(f,'Type','Axes');
     for i=1:length(ax)
         title(ax(i), jaccardPlotLabels{length(ax)-i+1})
-    end
-    if param==2
-        ylim([0.5, 1]);
     end
 end
