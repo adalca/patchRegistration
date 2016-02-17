96805d4c96989bfcac901a01b8dbc3f10803624a
Andreea Bobu <abobu@cassia.csail.mit.edu>
2016-02-17 13:49:18 -0500

    wrote mcc register to raw file

---
+++ src/analysis/reg2statsSubjectNii.m
@@ -0,0 +1,43 @@
+function reg2statsSubjectNii(regfolder)
+    try
+        pathsFile = [regfolder, '/paths.ini'];
+        paths = ini2struct(pathsFile);
+        srcName = paths.sourceName;
+        tgtName = paths.targetName;
+        displName = sprintf('%s-2-%s-warp', srcName, tgtName);
+        displInvName = sprintf('%s-2-%s-invWarp', srcName, tgtName);
+        displFile = sprintf('%s.nii.gz', displName);
+        displInvFile = sprintf('%s.nii.gz', displInvName);
+        sourceWarpedFile = sprintf('%s-in-%s_via_%s.nii.gz', srcName, tgtName, displName);
+        targetWarpedFile = sprintf('%s-in-%s_via_%s.nii.gz', tgtName, srcName, displInvName);
+        sourceWarpedSegFile = sprintf('%s-seg-in-%s_via_%s.nii.gz', srcName, tgtName, displName);
+        targetWarpedSegFile = sprintf('%s-seg-in-%s_via_%s.nii.gz', tgtName, srcName, displInvName);
+
+        displ = niftireg.prepNiiToVol(fullfile(regfolder, 'final', displFile));
+        displInv = niftireg.prepNiiToVol(fullfile(regfolder, 'final', displInvFile));
+        sourceWarped = niftireg.prepNiiToVol(fullfile(regfolder, 'final', sourceWarpedFile));
+        targetWarped = niftireg.prepNiiToVol(fullfile(regfolder, 'final', targetWarpedFile));
+        sourceWarpedSeg = niftireg.prepNiiToVol(fullfile(regfolder, 'final', sourceWarpedSegFile));
+        targetWarpedSeg = niftireg.prepNiiToVol(fullfile(regfolder, 'final', targetWarpedSegFile));
+        sourceSeg = niftireg.prepNiiToVol(paths.sourceSegFile);
+        targetSeg = niftireg.prepNiiToVol(paths.targetSegFile);
+
+        % go through iteration files
+        alldicelabels = unique([sourceSeg(:); targetSeg(:)]);
+        [dicesSource, dicelabels] = ...
+            dice(sourceWarpedSeg, targetSeg, alldicelabels);
+        [jaccardsSource, jaccardlabels] = ...
+            jaccard(sourceWarpedSeg, targetSeg, alldicelabels);
+        [dicesTarget, dicelabels] = ...
+            dice(targetWarpedSeg, sourceSeg, alldicelabels);
+        [jaccardsTarget, jaccardlabels] = ...
+            jaccard(targetWarpedSeg, sourceSeg, alldicelabels);
+
+        savePath = [regfolder, '/out/stats.mat'];
+        % save stats
+        % TODO: also need stats for subject to subjectraw
+        save(savePath, 'dicesSource', 'dicelabels', 'jaccardsSource', 'jaccardlabels', 'dicesTarget', 'jaccardsTarget');
+    catch err
+        fprintf(1, 'skipping %d due to \n\t%s', i, err.identifier);
+    end
+end
