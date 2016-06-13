14af68c527b7123c9a2c393b13ff019b222321e0
Andreea Bobu <abobu@vision15.csail.mit.edu>
2016-06-13 16:45:18 -0400

    new mccReg2raw

--- mcc/mccReg2raw.m
+++ mcc/mccReg2raw.m
@@ -1,26 +1,28 @@
-function mccReg2raw(sourceDsXFile, sourceDsXUsXMaskFile, atlSeg2SubjRegNii, atlSeg2SubjRegMat, ...
+function nii = mccReg2raw(subjRawDsXFile, subjDsXUsXMaskFile, atlSegInSubjRegNii, subj2AtlRegMat, ...
     saveSourceRawSegNii)
 % affine propagation of segmentations from affine_registered_subject space (i.e. rough "atlas"
 % space) to original subject DsXUsX space.
 %
-% sourceDsXFile example: buckner01_brain_roc_downsampled5.nii.gz
-% sourceDsXUsXMaskFile example: buckner01_brain_downsampled5_reinterpolated5_dsmask.nii.gz
-% atlSeg2SubjRegNii example: sprintf('%s-seg-in-%s_via_%s.nii.gz', tgtName, srcName, displInvName);
-% atlSeg2SubjRegMat example: buckner01_brain_downsampled5_reinterpolated5_reg.mat
+% subjRawDsXFile example: buckner01_brain_roc_downsampled5.nii.gz
+% subjDsXUsXMaskFile example: buckner01_brain_downsampled5_reinterpolated5_dsmask.nii.gz
+% atlSegInSubjRegNii example: sprintf('%s-seg-in-%s_via_%s.nii.gz', tgtName, srcName, displInvName);
+% subj2AntlRegMat example: buckner01_brain_downsampled5_reinterpolated5_reg.mat
 % saveSourceRawSegNii example in output: sprintf('%s-seg-in-%s-raw_via_%s.nii.gz', tgtName, srcName, displInvName);
 
     % get tform and invert
-    load(atlSeg2SubjRegMat);
+    load(subj2AtlRegMat);
     tform = tform.invert;
     
     % this only makes sense one way. Need to process separately
     % took out 'outputSave', saveSourceRawSegNii  
-    swNii = warpNii(atlSeg2SubjRegNii, tform, 'nearest', 'OutputView', sourceDsXUsXMaskFile);
+    swNii = warpNii(atlSegInSubjRegNii, tform, 'nearest', 'OutputView', subjDsXUsXMaskFile);
     
     % make new nii in DsX space.
-    masknii = loadNii(sourceDsXUsXMaskFile);
-    nii = loadNii(sourceDsXFile);
+    masknii = loadNii(subjDsXUsXMaskFile);
+    nii = loadNii(subjRawDsXFile);
     sz = size(nii.img);
     nii.img = reshape(swNii.img(masknii.img(:)==1), sz);
-    saveNii(nii, saveSourceRawSegNii);
+    if nargin >= 5 && ~isempty(saveSourceRawSegNii)
+        saveNii(nii, saveSourceRawSegNii);
+    end
 end
