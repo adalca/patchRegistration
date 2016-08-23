7de2e5d71048d9e2685326a25452ce785239b04d
Adrian Dalca <adalca@mit.edu>
2016-08-23 19:45:05 -0400

    update setup data

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # PBR: Patch-Based Discrete Registration 
 Patch-based discrete registration for two n-dimensional volumes (e.g. images or medical 3D volumes).
 
-## 3D Medical Registration
-Run `registerNii` with example parameter and paths files in the `config` folder.
+## Quick Start
+To quickly register two volumes:
+- download package
+- download required libraries (specified in `setup.m`)
+- open and modify `setup.m`, adding necessary paths
+- modify example parameter file `config/params.ini`. Each parameter has a description.
+- modify example paths file `config/paths.ini` for the input and output files
+- run `registerNii()` to register volumes
 
-## 2D Image Registration
+### Parameter Files
+The `config` folder contains an example parameter file `params.ini`. All parameters are briefly described there.
+
+### 3D Medical Registration
+The `config` folder contains an example paths input file `paths.ini`. In essence, this file contains the path to the moving, fixed and output files. 
+Finally, run `registerNii('/paths/to/paths.ini', '/paths/to/params.ini')` to register the volumes. 
+
+### 2D Image Registration
 A wrapper for running 2D registration is in defelopment, but it should be quite easy to run `patchreg.multiscale` with a params file similar to the one in the `config` folder.
 
 ## Contact
 {adalca,abobu}@csail.mit.edu  
 
-**Reference**  
-A.V.Dalca, A.Bobu, N.Rost, P.Golland. Patch-Based Discrete Registration of Clinical Quality Brain Images. *In submission*
+## Reference  
+A.V. Dalca, A. Bobu, N.S. Rost, P. Golland. Patch-Based Discrete Registration of Clinical Brain Images 
+*To Appear In Proc. MICCAI-PATCHMI Patch-based Techniques in Medical Imaging*, LNCS 2016. 
--- setup.m
+++ setup.m
@@ -1,57 +1,28 @@
-% required toolboxes:
-% - adalca's main toolboxes: mgt, patchlib, mvit
-% - view3D (TODO: make this optional)
+%% patch registration setup
+% run this file to prepare the patch registration toolbox.
 
+%% required toolboxes
+% the following toolboxes are required for patchRegistration
 
-%% setup paths
+% mgh toolbox. available at https://github.com/adalca/mgt
+PATHS.mgt = '/path/to/mgt';
 
-% get username
-[~, whoami] = system('whoami');
-whoami = strrep(whoami, '\', '/');
-spl = strsplit(whoami, '/');
-usrname = spl{end}; 
+% mmit toolbox. available at https://github.com/adalca/mmit
+PATHS.mmit = '/path/to/mmit';
 
-% constants
-if ispc % windows paths
-    OUTPUT_PATH = 'D:/Dropbox (MIT)/Research/patchRegistration/output/';
-    if strncmp(usrname, 'abobu', 5) 
-        BUCKNER_PATH = 'Windows/path/to/data';
-        BUCKNER_ATLAS_PATH = '';
-        
-    else
-        assert(strncmp(usrname, 'adalca', 6) )
-        PREBUCKNER_PATH = 'D:/Dropbox (MIT)/Public/robert/buckner';
-        BUCKNER_PATH = 'D:\Dropbox (MIT)\Research\patchSynthesis\data\buckner\proc';
-        BUCKNER_ATLAS_PATH = 'D:\Dropbox (MIT)\Research\patchSynthesis\data\buckner\atlases\';
-        TOOLS_PATH = 'C:\Users\adalca\Dropbox (Personal)\MATLAB\toolboxes';
-        EXTLIB_PATH = 'C:\Users\adalca\Dropbox (Personal)\MATLAB\external_toolboxes';
-    end
+% mmit toolbox. available at https://github.com/adalca/patchlib
+PATHS.patchlib = '/path/to/patchlib';
 
-else % unix/mac paths
-    OUTPUT_PATH = '/data/vision/polina/scratch/abobu/patchRegistration/output/';
-    if strncmp(usrname, 'abobu', 5)
-        PREBUCKNER_PATH = '/afs/csail.mit.edu/u/a/abobu/toolbox/buckner/';
-        BUCKNER_PATH = '/data/vision/polina/scratch/adalca/patchSynthesis/data/buckner/proc/';
-        BUCKNER_ATLAS_PATH = '/data/vision/polina/scratch/adalca/patchSynthesis/data/buckner/atlases/';
-        TOOLS_PATH = '/afs/csail.mit.edu/u/a/abobu/toolbox';
-        EXTLIB_PATH = '/afs/csail.mit.edu/u/a/abobu/toolbox';
-    else
-        assert(strncmp(usrname, 'adalca', 6) )
-        BUCKNER_PATH = '/data/vision/polina/scratch/adalca/patchSynthesis/data/buckner/proc';
-        BUCKNER_ATLAS_PATH = '/data/vision/polina/scratch/adalca/patchSynthesis/data/buckner/atlases/';
-        TOOLS_PATH = '/data/vision/polina/users/adalca/MATLAB/toolboxes';
-        EXTLIB_PATH = '/data/vision/polina/users/adalca/MATLAB/external_toolboxes';
-    end
-end
+% ugm path. available at http://www.cs.ubc.ca/~schmidtm/Software/UGM.html.
+PATHS.ugm = '/path/to/UGM';
 
-%% add paths as necessary
-VIEW_PATH = fullfile(EXTLIB_PATH, 'view3D'); 
-UGM_PATH = fullfile(EXTLIB_PATH, 'UGM'); 
-addpath(genpath(TOOLS_PATH));
-addpath(genpath(VIEW_PATH));
-addpath(genpath(UGM_PATH));
-addpath(genpath(fileparts(mfilename('fullpath'))));
+% patch registration
+PATHS.patchRegistration = fileparts(mfilename('fullpath'));
 
+%% add paths
+allpaths = struct2cell(PATHS);
+addpath(genpath(allpaths));
 
 %% settings
-warning off backtrace; % turn off backtrace for warnings.
+% turn off backtrace for warnings.
+warning off backtrace; 
